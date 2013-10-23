d50f999a76b048fb46ce93d6f5fbf2a53c58794d
Xavier Serra <xavier.serra@upf.edu>
2013-10-23 09:34:50 +0200

    lecture changes

--- teaching-material/lectures/lect4-STFT/code-plots/spectrogram.py
+++ teaching-material/lectures/lect4-STFT/code-plots/spectrogram.py
@@ -1,12 +1,15 @@
 from pylab import *
 from scipy.io.wavfile import read
+import numpy as np
 
 (fs, x) = read('piano.wav')
 figure(1)
 subplot(2,1,1)
-specgram(x, NFFT=256, Fs=fs, noverlap=128)
+specgram(x, NFFT=256, window= np.hamming(256), Fs=fs, noverlap=128)
 axis([0,size(x)/fs,0,fs/4])
+title('N = 256, window = hamming(256), H = 128')
 subplot(2,1,2)
-specgram(x, NFFT=1024, Fs=fs, noverlap=128)
+specgram(x, NFFT=1024, window= np.hamming(1024), Fs=fs, noverlap=128)
 axis([0,size(x)/fs,0,fs/4])
+title('N = 1024, window = hamming(1024), H = 128')
 show()
---
+++ teaching-material/lectures/lect4-STFT/code-plots/window-overlap.py
@@ -0,0 +1,39 @@
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+
+
+plt.figure(1)
+N= 1000
+M = 201
+w = signal.blackman(M)
+w1 = w/sum(w)
+
+y = np.zeros(N)
+H = 100
+pin = 0
+pend = N - M
+plt.subplot(211)
+while pin<pend:
+	y [pin:pin+M] += w1*H
+	plt.plot(np.arange(pin, pin+M), w, 'b')
+	pin += H
+plt.plot(np.arange(0, N), y, 'r')
+plt.axis([0, N-H, 0, max(y)+.01])
+plt.title('Blackman, M=201, H=100')
+
+y = np.zeros(N)
+H = 50
+pin = 0
+pend = N - M
+plt.subplot(212)
+while pin<pend:
+	y [pin:pin+M] += w1*H
+	plt.plot(np.arange(pin, pin+M), w, 'b')
+	pin += H
+plt.plot(np.arange(0, N), y, 'r')
+plt.axis([0, N-H, 0, max(y)+.01])
+plt.title('Blackman, M=201, H=50')
+
+plt.show()
+
