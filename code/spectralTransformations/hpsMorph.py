6a84d6d6bf7b25c0b2c04145a6cb3a0cf9577ce6
Xavier Serra <xavier.serra@upf.edu>
2013-11-15 22:31:08 +0100

    small changes

--- code/spectralTransformations/hpsMorph.py
+++ code/spectralTransformations/hpsMorph.py
@@ -9,8 +9,6 @@ import sys, os, functools
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../basicFunctions/'))
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../basicFunctions_C/'))
 
-#sys.path.append(os.path.realpath('../basicFunctions/'))
-#sys.path.append(os.path.realpath('../basicFunctions_C/'))
 import smsF0DetectionTwm as fd
 import smsWavplayer as wp
 import smsPeakProcessing as PP
