06089bbdedf3670c7ff5ca5f857ca4d5422c2cc1
Xavier Serra <xavier.serra@upf.edu>
2014-01-24 18:25:45 +0100

    some text changes

--- teaching-material/lectures/lect1-Introduction/code-plots/complex-sinewave.py
+++ teaching-material/lectures/lect1-Introduction/code-plots/complex-sinewave.py
@@ -6,8 +6,10 @@ k = 3
 plt.figure(1)
 
 s = np.exp(1j*2*np.pi*k/N*np.arange(-N/2, N/2))
-plt.plot(np.arange(-N/2, N/2), np.real(s), 'r')
-plt.title ('red: real part; blue: imaginary part')
-plt.plot(np.arange(-N/2, N/2), np.imag(s),'b')
+plt.plot(np.arange(-N/2, N/2), np.real(s), 'r', label="real")
+plt.plot(np.arange(-N/2, N/2), np.imag(s),'b', label="imaginary")
+plt.xlabel('time')
+plt.ylabel('amplitude')
 plt.axis([-N/2,N/2,-1,1])
+plt.legend()
 plt.show()
