75929abf4785f8e226b554f6ee538383a4d87598
Xavier Serra <xavier.serra@upf.edu>
2014-03-07 23:25:57 +0100

    changes to lect1

--- lectures/1-Introduction/plots-code/sinewave.py
+++ lectures/1-Introduction/plots-code/sinewave.py
@@ -7,7 +7,7 @@ phi = np.pi/2
 fs = 100
 t = np.arange(-1, 1, 1.0/fs)
 x = A * np.cos(2*np.pi*f0*t+phi)
-plt.plot(t, x)
+plt.plot(t, x, linewidth=2)
 plt.axis([-1,1,-0.8,0.8])
 plt.xlabel('time')
 plt.ylabel('amplitude')
