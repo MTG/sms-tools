b3a8bbb53aafc17cb3c22cb3bb89b235c052bae7
fabiaserra <fabia.serra92@gmail.com>
2014-07-13 14:29:31 +0200

    Add GUI files

---
+++ gui/GUI.py
@@ -0,0 +1,41 @@
+from Tkinter import *
+from notebook import *
+from Dft_frame import *
+from Stft_frame import *
+
+root = Tk( ) 
+root.title('sms-tools GUI')
+nb = notebook(root, TOP) # make a few diverse frames (panels), each using the NB as 'master': 
+
+# uses the notebook's frame
+f1 = Frame(nb( )) 
+dft = Dft_frame(f1)
+
+f2 = Frame(nb( )) 
+stft = Stft_frame(f2)
+#b2 = Button(f2, text='Button 2') 
+#b3 = Button(f2, text='Beep 2', command=Tk.bell) 
+#b2.pack(fill=BOTH, expand=1) 
+#b3.pack(fill=BOTH, expand=1) 
+
+f3 = Frame(nb( )) 
+f4 = Frame(nb( )) 
+f5 = Frame(nb( )) 
+f6 = Frame(nb( )) 
+f7 = Frame(nb( )) 
+f8 = Frame(nb( )) 
+f9 = Frame(nb( )) 
+
+nb.add_screen(f1, "DFT Model") 
+nb.add_screen(f2, "STFT")
+nb.add_screen(f3, "Sine Model") 
+nb.add_screen(f4, "Harmonic Model") 
+nb.add_screen(f5, "Stochastic Model") 
+nb.add_screen(f6, "SPR Model") 
+nb.add_screen(f7, "SPS Model") 
+nb.add_screen(f8, "HPR Model") 
+nb.add_screen(f9, "HPS Model") 
+
+nb.display(f1)
+
+root.mainloop( )
---
+++ gui/stft_frame.py
@@ -0,0 +1,138 @@
+from Tkinter import *
+import tkFileDialog, tkMessageBox
+import sys, os
+import pygame
+from scipy.io.wavfile import read
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../examples/'))
+import stft_example
+ 
+class Stft_frame:
+  
+    def __init__(self, parent):  
+         
+        self.parent = parent        
+        self.initUI()
+        pygame.init()
+
+    def initUI(self):
+
+        choose_label = "Choose an input audio file .wav (monophonic with sampling rate of 44100 Hz):"
+        Label(self.parent, text=choose_label).grid(row=0, column=0, sticky=W, padx=5, pady=(10,2))
+ 
+        #TEXTBOX TO PRINT PATH OF THE SOUND FILE
+        self.filelocation = Entry(self.parent)
+        self.filelocation.focus_set()
+        self.filelocation["width"] = 60
+        self.filelocation.grid(row=1,column=0, columnspan=3, sticky=W+E, padx=10)
+        self.filelocation.delete(0, END)
+        self.filelocation.insert(0, '../sounds/piano.wav')
+
+        #BUTTON TO BROWSE SOUND FILE
+        self.open_file = Button(self.parent, text="Browse...", command=self.browse_file) #see: def browse_file(self)
+        self.open_file.grid(row=1, column=3, padx=2) #put it beside the filelocation textbox
+ 
+        #BUTTON TO PREVIEW SOUND FILE
+        self.preview = Button(self.parent, text=">", command=self.preview_sound, bg="gray30", fg="white")
+        self.preview.grid(row=1, column=4, padx=(2,6))
+
+        ## STFT MODEL
+
+        #ANALYSIS WINDOW TYPE
+        wtype_label = "Analysis window type (choice of rectangular, hanning, hamming, blackman, blackmanharris):"
+        Label(self.parent, text=wtype_label).grid(row=2, column=0, sticky=W, padx=5, pady=(10,2))
+        self.w_type = StringVar()
+        self.w_type.set("hamming") # initial value
+        window_option = OptionMenu(self.parent, self.w_type, "rectangular", "hanning", "hamming", "blackman", "blackmanharris")
+        window_option.grid(row=3, column=0, sticky=W, padx=10)
+
+        #WINDOW SIZE
+        wsize_label = "Analysis window size M (odd integer value, e.g. 1024):"
+        Label(self.parent, text=wsize_label).grid(row=4, column=0, sticky=W, padx=5, pady=(10,2))
+        self.M_entry = Entry(self.parent, justify=CENTER)
+        self.M_entry["width"] = 8
+        self.M_entry.grid(row=5,column=0, sticky=W, padx=10)
+        self.M_entry.delete(0, END)
+        self.M_entry.insert(0, "1024")
+
+        #FFT SIZE
+        fft_label = "FFT size N (power of two, bigger than M, e.g. 1024):"
+        Label(self.parent, text=fft_label).grid(row=6, column=0, sticky=W, padx=5, pady=(10,2))
+        self.N_entry = Entry(self.parent, justify=CENTER)
+        self.N_entry["width"] = 8
+        self.N_entry.grid(row=7,column=0, sticky=W, padx=10)
+        self.N_entry.delete(0, END)
+        self.N_entry.insert(0, "1024")
+
+        #HOP SIZE
+        hop_label = "Hop size H (at least 1/2 of analysis window size to have good overlap-add, e.g. 512):"
+        Label(self.parent, text=hop_label).grid(row=8, column=0, sticky=W, padx=5, pady=(10,2))
+        self.H_entry = Entry(self.parent, justify=CENTER)
+        self.H_entry["width"] = 8
+        self.H_entry.grid(row=9, column=0, sticky=W, padx=10)
+        self.H_entry.delete(0, END)
+        self.H_entry.insert(0, "512")
+
+        #BUTTON TO COMPUTE EVERYTHING
+        self.compute = Button(self.parent, text="Compute", command=self.compute_model, bg="dark red", fg="white")
+        self.compute.grid(row=10, column=0, padx=5, pady=(20,2), sticky=W)
+
+        #BUTTON TO PLAY OUTPUT
+        output_label = "Output:"
+        Label(self.parent, text=output_label).grid(row=11, column=0, sticky=W, padx=5, pady=(20,2))
+        self.output = Button(self.parent, text=">", command=self.play_out_sound, bg="gray30", fg="white")
+        self.output.grid(row=11, column=0, padx=(70,5), pady=(20,2), sticky=W)
+
+
+        # define options for opening file
+        self.file_opt = options = {}
+        options['defaultextension'] = '.wav'
+        options['filetypes'] = [('All files', '.*'), ('Wav files', '.wav')]
+        options['initialdir'] = '../sounds/'
+        options['title'] = 'Open a mono audio file .wav with sample frequency 44100 Hz'
+
+    def preview_sound(self):
+        
+        filename = self.filelocation.get()
+
+        if filename[-4:] == '.wav':
+            (fs, x) = read(filename)
+        else:
+            tkMessageBox.showerror("Wav file", "The audio file must be a .wav")
+            return
+
+        if len(x.shape) > 1 :
+            tkMessageBox.showerror("Stereo file", "Audio file must be Mono not Stereo")
+        elif fs != 44100:
+            tkMessageBox.showerror("Sample Frequency", "Sample frequency must be 44100 Hz")
+        else:
+            sound = pygame.mixer.Sound(filename)
+            sound.play()
+ 
+    def browse_file(self):
+        
+        self.filename = tkFileDialog.askopenfilename(**self.file_opt)
+ 
+        #set the text of the self.filelocation
+        self.filelocation.delete(0, END)
+        self.filelocation.insert(0,self.filename)
+
+    def compute_model(self):
+        
+        try:
+            M = int(self.M_entry.get())
+            N = int(self.N_entry.get())
+            H = int(self.H_entry.get())
+            stft_example.main(self.filelocation.get(), self.w_type.get(), M, N, H)
+
+        except ValueError:
+            tkMessageBox.showerror("Input values error", "Some parameters are incorrect")
+
+    def play_out_sound(self):
+
+        filename = 'output_sounds/' + os.path.basename(self.filelocation.get())[:-4] + '_stft.wav'
+        if os.path.isfile(filename):
+            sound = pygame.mixer.Sound(filename)
+            sound.play()
+        else:
+            tkMessageBox.showerror("Output audio file not found", "The output audio file has not been computed yet")
