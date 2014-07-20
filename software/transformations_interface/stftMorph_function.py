e00fdd6c7ab65bfec34a3654a5ec5bbde75d7ea1
Xavier Serra <xavier.serra@upf.edu>
2014-07-20 22:54:06 +0200

    changed the stft transformation function

--- software/transformations_interface/stftMorph_function.py
+++ software/transformations_interface/stftMorph_function.py
@@ -1,4 +1,4 @@
-# function call to the transformation function of relevance to the stft
+# function for doing a morph between two sounds using the stft
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,16 +10,16 @@ import stft as STFT
 import utilFunctions as UF
 import stftTransformations as STFTT
 
-def main (inputFile1='../../sounds/ocean.wav', inputFile2='../../sounds/speech-male.wav', window1='hamming',  window2='hamming', 
+def main(inputFile1='../../sounds/ocean.wav', inputFile2='../../sounds/speech-male.wav', window1='hamming',  window2='hamming', 
 	M1=1024, M2=1024, N1=1024, N2=1024, H1=256, smoothf = .5, balancef = 0.2):
-	
+	# function to perform a morph between two sounds
 	# inputFile1: name of input sound file to be used as source
 	# inputFile2: name of input sound file to be used as filter
 	# window1 and window2: windows for both files
 	# M1 and M2: window sizes for both files
 	# N1 and N2: fft sizes for both sounds
 	# H1: hop size for sound 1 (the one for sound 2 is computed automatically)
-	# smoothf: smoothing factor to be applyed to sound 2
+	# smoothf: smoothing factor to be applyed to magnitude spectrum of sound 2 before morphing
 	# balancef: balance factor between booth sounds, 0 is sound 1 and 1 is sound 2
 
 	# read input sounds
@@ -40,7 +40,7 @@ def main (inputFile1='../../sounds/ocean.wav', inputFile2='../../sounds/speech-m
 	mY, pY = STFT.stftAnal(y, fs, w1, N1, H1)
 	
 	# write output sound
-	outputFile = 'output_sounds/' + os.path.basename(inputFile1)[:-4] + '_stftTransformation.wav'
+	outputFile = 'output_sounds/' + os.path.basename(inputFile1)[:-4] + '_stftMorph.wav'
 	UF.wavwrite(y, fs, outputFile)
 
 	# --------- plotting --------------------
@@ -51,7 +51,7 @@ def main (inputFile1='../../sounds/ocean.wav', inputFile2='../../sounds/speech-m
 	# frequency range to plot
 	maxplotfreq = 10000.0
 
-	# plot the input sound
+	# plot sound 1
 	plt.subplot(4,1,1)
 	plt.plot(np.arange(x1.size)/float(fs), x1)
 	plt.axis([0, x1.size/float(fs), min(x1), max(x1)])
@@ -59,7 +59,7 @@ def main (inputFile1='../../sounds/ocean.wav', inputFile2='../../sounds/speech-m
 	plt.xlabel('time (sec)')
 	plt.title('input sound: x')
 
-		# plot magnitude spectrogram of input sound 
+	# plot magnitude spectrogram of sound 1
 	plt.subplot(4,1,2)
 	numFrames = int(mX1[:,0].size)
 	frmTime = H1*np.arange(numFrames)/float(fs)                             
@@ -70,7 +70,7 @@ def main (inputFile1='../../sounds/ocean.wav', inputFile2='../../sounds/speech-m
 	plt.title('magnitude spectrogram of x')
 	plt.autoscale(tight=True)
 
-	# plot magnitude spectrogram of output sound 
+	# plot magnitude spectrogram of morphed sound 
 	plt.subplot(4,1,3)
 	numFrames = int(mY[:,0].size)
 	frmTime = H1*np.arange(numFrames)/float(fs)                             
@@ -81,7 +81,7 @@ def main (inputFile1='../../sounds/ocean.wav', inputFile2='../../sounds/speech-m
 	plt.title('magnitude spectrogram of y')
 	plt.autoscale(tight=True)
 
-	# plot the output sound
+	# plot the morphed sound
 	plt.subplot(4,1,4)
 	plt.plot(np.arange(y.size)/float(fs), y)
 	plt.axis([0, y.size/float(fs), min(y), max(y)])
