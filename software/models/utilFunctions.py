078c3d423e12ec0bfbdd8e6045c81510a003bd15
Sankalp Gulati <sankalp.gulati@upf.edu>
2014-09-10 18:06:06 +0200

    System exit if Cython modules are not imported
    Added a condition that if cython modules are not imported system exits. Also removed functions printing warning and error msgs (were not useful)

--- software/models/utilFunctions.py
+++ software/models/utilFunctions.py
@@ -7,24 +7,15 @@ sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), './uti
 try:
 	import utilFunctions_C as UF_C
 except ImportError:
-	print "C modules could not be imported because there were not compiled."
-
-
-def printError(errorID):
-	if errorID == 1:
-		print "Error opening file"
-				
-def printWarning(warningID):
-	if warningID ==1:
-		print "\n"
-		print "-------------------------------------------------------------------------------"
-		print "Warning:"
-		print "Cython modules for some of the core functions were not imported."
-		print "The processing might be significantly slower in such case"
-		print "Please refer to the README file for instructions to compile cython modules"
-		print "https://github.com/MTG/sms-tools/README.md"
-		print "-------------------------------------------------------------------------------"
-		print "\n"
+	print "\n"
+	print "-------------------------------------------------------------------------------"
+	print "Warning:"
+	print "Cython modules for some of the core functions were not imported."
+	print "Please refer to the README file for the instructions to compile the cython modules"
+	print "Exiting the code!!"
+	print "-------------------------------------------------------------------------------"
+	print "\n"
+	sys.exit(0)
 
 def isPower2(num):
 	"""
