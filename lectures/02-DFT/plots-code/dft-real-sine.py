0af729b810065cc7ea6ebb0f2e0d84edd55471b8
xavier <xavier.serra@upf.edu>
2017-01-13 16:04:33 +0100

    deleted not needed real function

--- lectures/02-DFT/plots-code/dft-real-sine.py
+++ lectures/02-DFT/plots-code/dft-real-sine.py
@@ -12,7 +12,7 @@ x = np.cos(2*np.pi*k0/N*nv)
 
 plt.subplot(311)
 plt.title('x; k_0 = 7.5, N = 64')
-plt.plot(nv, np.real(x),'b', lw=1.5)
+plt.plot(nv, x,'b', lw=1.5)
 plt.axis([-N/2,N/2-1,-1,1])
 for k in kv:
 	s = np.exp(1j*2*np.pi*k/N*nv)
