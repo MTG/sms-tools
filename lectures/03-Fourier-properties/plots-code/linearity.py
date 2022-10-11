165e43ffc8eae592cc5088f99a3e741514b434ec
Xavier Serra <xavier.serra@upf.edu>
2022-10-11 11:48:36 +0200

    added to gitignore the .idea folder fixd plot for linearity property

--- lectures/03-Fourier-properties/plots-code/linearity.py
+++ lectures/03-Fourier-properties/plots-code/linearity.py
@@ -13,12 +13,12 @@ x2 = b*np.exp(1j*2*np.pi*k2/N*np.arange(N))
 
 plt.figure(1, figsize=(9.5, 7))
 plt.subplot(321)
-plt.title('x1 (amp=.5, freq=20)')
+plt.title('real(x1) (amp=.5, freq=20)')
 plt.plot(np.arange(0, N, 1.0), np.real(x1), lw=1.5)
 plt.axis([0, N, -1, 1])
 
 plt.subplot(322)
-plt.title('x2 (amp=1, freq=25)')
+plt.title('real(x2) (amp=1, freq=25)')
 plt.plot(np.arange(0, N, 1.0), np.real(x2), lw=1.5)
 plt.axis([0, N, -1, 1])
 X1 = fft(x1)
@@ -26,27 +26,27 @@ mX1 = abs(X1)/N
 
 plt.subplot(323)
 plt.title('mX1 (amp=.5, freq=20)')
-plt.plot(np.arange(0, N, 1.0), mX1, 'r', lw=1.5)
+plt.plot(np.arange(0, N, 1.0), mX1, 'r-x', lw=1.5)
 plt.axis([0,N,0,1])
 X2 = fft(x2)
 mX2 = abs(X2)/N        
 
 plt.subplot(324)
 plt.title('mX2 (amp=1, freq=25)')
-plt.plot(np.arange(0, N, 1.0), mX2, 'r', lw=1.5)
+plt.plot(np.arange(0, N, 1.0), mX2, 'r-x', lw=1.5)
 plt.axis([0,N,0,1])
 x = x1 + x2
 
 plt.subplot(325)
 plt.title('mX1+mX2')
-plt.plot(np.arange(0, N, 1.0), mX1+mX2, 'r', lw=1.5)
+plt.plot(np.arange(0, N, 1.0), mX1+mX2, 'r-x', lw=1.5)
 plt.axis([0, N, 0, 1])
 X = fft(x)
 mX= abs(X)/N        
 
 plt.subplot(326)
 plt.title('DFT(x1+x2)')
-plt.plot(np.arange(0, N, 1.0), mX, 'r', lw=1.5)
+plt.plot(np.arange(0, N, 1.0), mX, 'r-x', lw=1.5)
 plt.axis([0,N,0,1])
 
 plt.tight_layout()
