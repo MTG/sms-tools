683b4a23b5abd58404b04ad473e29407d71605c2
Xavier Serra <xavier.serra@upf.edu>
2023-01-25 12:58:00 +0100

    removed the -1 from
    INT16_FAC = (2**15)-1
    INT32_FAC = (2**31)-1
    INT64_FAC = (2**63)-1

--- software/models/utilFunctions.py
+++ software/models/utilFunctions.py
@@ -38,9 +38,9 @@ def isPower2(num):
     return ((num & (num - 1)) == 0) and num > 0
 
 
-INT16_FAC = (2 ** 15) - 1
-INT32_FAC = (2 ** 31) - 1
-INT64_FAC = (2 ** 63) - 1
+INT16_FAC = (2 ** 15)
+INT32_FAC = (2 ** 31)
+INT64_FAC = (2 ** 63)
 norm_fact = {'int16': INT16_FAC, 'int32': INT32_FAC, 'int64': INT64_FAC, 'float32': 1.0, 'float64': 1.0}
 
 
